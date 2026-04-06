c77d7b850b26c98ffc7881bc15451f93ae9caacd
enzo-6464342 <enzorodriguesdemoura11@gmail.com>
2026-04-06 17:56:40 -0300

    exerciciosPHP

---
+++ atividadd.php
@@ -0,0 +1,15 @@
+<?php
+$numero = 7;
+echo "tabuada do 7 $numero: <br>";
+echo "$numero x 0  = " . ($numero * 0) . "<br>";
+echo "$numero x 1  = " . ($numero * 1) . "<br>";
+echo "$numero x 2  = " . ($numero * 2) . "<br>";
+echo "$numero x 3  = " . ($numero * 3) . "<br>";
+echo "$numero x 4  = " . ($numero * 4) . "<br>";
+echo "$numero x 5  = " . ($numero * 5) . "<br>";
+echo "$numero x 6  = " . ($numero *¨6) . "<br>";
+echo "$numero x 7  = " . ($numero * 7) . "<br>";
+echo "$nuemro x 8  = " . ($numero * 8) . "<br>";
+echo "$numero x 9  = " . ($numero * 9) . "<br>";
+echo "$numero x 10 = " . ($numero * 10) . "<br>";
+?> 
--- atividade02.php
+++ atividade02.php
@@ -2,4 +2,5 @@
 $nome = "Ana Clara";
 echo "Olá! <br> Meu Nome é $nome e estou aprendendo PHP";
 
+
 ?>
